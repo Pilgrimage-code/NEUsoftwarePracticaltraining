bd7eb47348adfbe42fb2cc2397f2a00b8c4020ae
Pilgrimage-code <shaoyanzhe_2005@qq.com>
2025-07-01 16:25:11 +0800

    云端兼容性修复

--- cemh-backend/src/main/java/com/cemh/utils/AliyunOSSOperator.java
+++ cemh-backend/src/main/java/com/cemh/utils/AliyunOSSOperator.java
@@ -11,6 +11,7 @@ import java.io.ByteArrayInputStream;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.UUID;
+import com.aliyun.oss.common.auth.DefaultCredentialProvider;
 
 @Component
 public class AliyunOSSOperator {
@@ -33,7 +34,8 @@ public class AliyunOSSOperator {
 
     public String upload(byte[] content, String originalFilename) throws Exception {
         // 从环境变量中获取访问凭证。运行本代码示例之前，请确保已设置环境变量OSS_ACCESS_KEY_ID和OSS_ACCESS_KEY_SECRET。
-        EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+        //EnvironmentVariableCredentialsProvider credentialsProvider = CredentialsProviderFactory.newEnvironmentVariableCredentialsProvider();
+        DefaultCredentialProvider defaultCredentialProvider = new DefaultCredentialProvider(accessKeyId, accessKeySecret);
 
         // 填写Object完整路径，例如202406/1.png。Object完整路径中不能包含Bucket名称。
         //获取当前系统日期的字符串,格式为 yyyy/MM
@@ -43,14 +45,15 @@ public class AliyunOSSOperator {
         String objectName = dir + "/" + newFileName;
 
         // 创建OSSClient实例。
-        ClientBuilderConfiguration clientBuilderConfiguration = new ClientBuilderConfiguration();
-        clientBuilderConfiguration.setSignatureVersion(SignVersion.V4);
-        OSS ossClient = OSSClientBuilder.create()
-                .endpoint(endpoint)
-                .credentialsProvider(credentialsProvider)
-                .clientConfiguration(clientBuilderConfiguration)
-                .region(region)
-                .build();
+//        ClientBuilderConfiguration clientBuilderConfiguration = new ClientBuilderConfiguration();
+//        clientBuilderConfiguration.setSignatureVersion(SignVersion.V4);
+//        OSS ossClient = OSSClientBuilder.create()
+//                .endpoint(endpoint)
+//                .credentialsProvider(credentialsProvider)
+//                .clientConfiguration(clientBuilderConfiguration)
+//                .region(region)
+//                .build();
+        OSS ossClient = new OSSClient(endpoint, defaultCredentialProvider);
 
         try {
             ossClient.putObject(bucketName, objectName, new ByteArrayInputStream(content));
