91d482b0d99b545fdeae1dcea30dc0b335294194
walenmacle <1948673480@qq.com>
2025-07-11 22:36:28 +0800

    Update PageResult.java

--- cemh-backend/src/main/java/com/cemh/common/PageResult.java
+++ cemh-backend/src/main/java/com/cemh/common/PageResult.java
@@ -11,7 +11,7 @@ import java.util.List;
  * 
  * @author 测盟汇技术团队
  * @version 1.0.0
- * @since 2024-06-15
+ * @since 2025-6-28
  */
 @Schema(description = "分页结果")
 public class PageResult<T> implements Serializable {
