c29768c51dd5d1a8980f3f328cfdbd743c4a2995
walenmacle <1948673480@qq.com>
2025-07-11 22:38:42 +0800

    Update RedisConfig.java

--- cemh-backend/src/main/java/com/cemh/config/RedisConfig.java
+++ cemh-backend/src/main/java/com/cemh/config/RedisConfig.java
@@ -17,7 +17,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
  * 
  * @author 测盟汇技术团队
  * @version 1.0.0
- * @since 2024-06-15
+ * @since 2025-6-28
  */
 @Configuration
 public class RedisConfig {
